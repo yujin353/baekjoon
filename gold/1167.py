013dbcdb568512f1a319d582220218d886a3a4d2
yujin353 <kimyujin0331@naver.com>
2023-07-06 14:37:36 +0900

    BFS-g2__main

--- gold/1167.py
+++ gold/1167.py
@@ -2,6 +2,9 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+v = None
+cv = None
+
 def BFS(n):
     visited = [False] * (v+1)
     visited[n] = True
@@ -20,12 +23,18 @@ def BFS(n):
     
     return diameter
 
-v = int(input())
-cv = [[] for _ in range(v+1)]
-for i in range(v):
-    nums = list(map(int, input().split()))
-    n = nums[0]
-    for j in range(1,len(nums)-1,2):
-        cv[n].append([nums[j], nums[j+1]])
+def main():
+    global v, cv
+    
+    v = int(input())
+    cv = [[] for _ in range(v+1)]
+    for i in range(v):
+        nums = list(map(int, input().split()))
+        n = nums[0]
+        for j in range(1,len(nums)-1,2):
+            cv[n].append([nums[j], nums[j+1]])
 
-print(max(BFS(BFS(1).index(max(BFS(1))))))
+    print(max(BFS(BFS(1).index(max(BFS(1))))))
+
+if __name__ == '__main__':
+    main()
