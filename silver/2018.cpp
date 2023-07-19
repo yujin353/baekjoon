eb282890fecc722db1c75608985a49f7ae24c0c4
yujin353 <kimyujin0331@naver.com>
2023-07-19 14:29:37 +0900

    twoPointer-s5

--- silver/2018.cpp
+++ silver/2018.cpp
@@ -3,33 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int cnt = 1;
-    int start = 1;
-    int end = 1;
-    int accum = 0;
-    
-    cin >> n;
-    while(start < n/2+1)
-    {
-        accum += end;
-        if (accum > n)
-        {
-            start++;
-            end = start;
-            accum = 0;
-        }
-        else if (accum == n)
-        {
-            cnt++;
-            start++;
-            end = start;
-            accum = 0;
-        }
-        else
-        {
-            end++;
-        }
-    }
-    cout << cnt << endl;
+	int n;
+	cin >> n;
+
+	int start = 1, end = 1, cnt = 0, sum = 1;
+	while(start <= n && end <= n) {
+		if (sum == n) {
+			cnt++;
+			end++;
+			sum += end;
+		}
+		else if (sum > n){
+			sum -= start;
+			start++;
+		}
+		else {
+			end++;
+			sum += end;
+		}
+	}
+	cout << cnt << endl;
 }
